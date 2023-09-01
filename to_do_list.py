f29f220fc19a015f36db1782ba01043d0ff41a92
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-09-01 12:58:57 +0300

    view_task methos

--- to_do_list.py
+++ to_do_list.py
@@ -15,3 +15,9 @@ class ToDoList:
     def __init__(self):
         self.tasks = []
 
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def view_tasks(self):
+        for task in self.tasks:
+            print(task)
